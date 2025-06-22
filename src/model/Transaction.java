e42abd1b50ba6d377aa9b8629bb3f4d11326b516
Mouhcine Ayar <hidden@example.com>
2025-06-22 12:48:46 +0100

    Initial commit - Banking System project

---
+++ src/model/Transaction.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Date;
+
+public class Transaction {
+	int transactionId;
+	int accNum;
+	Date date;
+	float amount;
+	String transactionType;
+	String toString();
+}
